0fc1433e11f4c8348868064398d075b466bb40d5
Leo Bernard <admin@leolabs.org>
2018-09-23 17:56:44 +0200

    :recycle: Move index styles into separate folder

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <App />,
