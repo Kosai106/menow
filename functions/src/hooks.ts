4fc470880512c1752c9ff1dfe82c272338e8724f
Moritz Gunz <moritz.gunz@gmail.com>
2018-09-23 19:47:43 +0200

    :bug: Fix auth middleware not receiving params

--- functions/src/hooks.ts
+++ functions/src/hooks.ts
@@ -13,25 +13,23 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(auth);
-
-app.get('/:user/:token/:type/set', handlify(async (req: express.Request, res: express.Response) => {
+app.get('/:user/:token/:type/set', auth, handlify(async (req: express.Request, res: express.Response) => {
   const { user, type } = req.params;
   return res.json({ newStatus: (await setOrUpdate(user, type, req.query)) });
 }));
 
-app.post('/:user/:token/:type/set', handlify(async (req: express.Request, res: express.Response) => {
+app.post('/:user/:token/:type/set', auth, handlify(async (req: express.Request, res: express.Response) => {
   const { user, type, ...status } = req.body;
   return res.json({ newStatus: (await setOrUpdate(user, type, status)) });
 }));
 
-app.get('/:user/:token/:type/unset', handlify(async (req: express.Request, res: express.Response) => {
+app.get('/:user/:token/:type/unset', auth, handlify(async (req: express.Request, res: express.Response) => {
   const { user, type } = req.params;
   await unset(user, type, req.query.save);
   return res.status(204).send();
 }));
 
-app.post('/:user/:token/:type/unset', handlify(async (req: express.Request, res: express.Response) => {
+app.post('/:user/:token/:type/unset', auth, handlify(async (req: express.Request, res: express.Response) => {
   const { user, type, save } = req.body;
   await unset(user, type, save);
   return res.status(204).send();
