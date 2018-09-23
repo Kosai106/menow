4ef19178d94ea580cd4180741b09fa073e576141
Moritz Gunz <moritz.gunz@gmail.com>
2018-09-23 13:38:35 +0200

    :hammer: Move business logic into model subfolder

--- functions/src/hooks.ts
+++ functions/src/hooks.ts
@@ -1,13 +1,10 @@
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
-import * as firebase from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-import { defaultStatus, Status } from '../../shared/types';
-
+import { setOrUpdate, unset } from './model/status';
 import auth from './util/auth';
-import { firestore } from './util/firestore';
 import { handlify } from './util/handlify';
 
 const app = express();
@@ -17,61 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(auth);
-
-const setOrUpdate = async (user: string, type: string, status: Partial<Status>) => {
-    const { added = null, ...newStatus }: Partial<Status> = {
-        ...defaultStatus,
-        ...status,
-        type,
-        updated: firebase.firestore.Timestamp.now(),
-    };
-
-    const statusRef = firestore.collection('users')
-        .doc(user)
-        .collection('current_status')
-        .doc(newStatus.type);
-
-    const currentStatus = await statusRef.get();
-
-    if (!currentStatus.exists) {
-        await statusRef.set({
-            ...newStatus,
-            added: firebase.firestore.Timestamp.now(),
-        });
-    } else {
-        await statusRef.update(newStatus);
-    }
-
-    return (await statusRef.get()).data();
-};
-
-const unset = async (user: string, type: string, save: boolean) => {
-    const statusRef = firestore.collection('users')
-        .doc(user)
-        .collection('current_status')
-        .doc(type);
-
-    const currentStatus = await statusRef.get();
-
-    const batch = firestore.batch();
-    batch.delete(statusRef);
-
-    if (currentStatus.exists && (save || (save === undefined && currentStatus.data().save))) {
-        const historyRef = firestore.collection('users')
-            .doc(user)
-            .collection('current_status')
-            .doc();
-
-        batch.set(historyRef, {
-            ...currentStatus.data(),
-            removed: firebase.firestore.Timestamp.now(),
-        });
-
-        return true;
-    }
-
-    return false;
-};
 
 app.get('/:user/:token/:type/set', handlify(async (req: express.Request, res: express.Response) => {
     const { user, type } = req.params;
