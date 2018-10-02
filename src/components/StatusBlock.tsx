2f8874bf800897aa01f581a82cd075f5141fcb67
Moritz Gunz <moritz.gunz@gmail.com>
2018-10-02 20:38:28 +0200

    :hammer: Rewrite StatusBlock as SFC

--- src/components/StatusBlock.tsx
+++ src/components/StatusBlock.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import * as React from "react";
 
 import { Status } from "../../shared/types";
@@ -5,27 +6,27 @@ import { Status } from "../../shared/types";
 import './StatusBlock.css';
 
 interface StatusProps {
+  className?: string;
   status: Status;
 }
 
-export default class StatusBlock extends React.Component<StatusProps> {
-  render() {
-    return (
-      <div className="status">
-        <h2>{this.props.status.icon}</h2>
-        <div className="text">
-          <h3>{this.props.status.text}</h3>
-          {this.props.status.link
-            ? <a
-              href={this.props.status.link}
-              rel="nofollow"
-              target="_blank"
-              style={{color: this.props.status.color || 'rgb(0, 111, 197)'}}>
-                {this.props.status.link_text || 'Learn more...'}
-              </a>
-            : null}
-        </div>
-      </div>
-    )
-  }
-}
+const StatusBlock: React.SFC<StatusProps> = props => (
+  <div className={classNames(props.className, 'status')}>
+    <h2>{props.status.icon}</h2>
+    <div className="text">
+      <h3>{props.status.text}</h3>
+
+      {props.status.link &&
+        <a
+          href={props.status.link}
+          rel="nofollow"
+          target="_blank"
+          style={{color: props.status.color || 'rgb(0, 111, 197)'}}
+        >
+          {props.status.link_text || 'Learn more...'}
+        </a>}
+    </div>
+  </div>
+);
+
+export default StatusBlock;
